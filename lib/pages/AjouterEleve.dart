53b677c99fbf0ff26bd0cc70c58d808760fa0409
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-05-24 10:11:37 +0000

    nouveau formulaire

--- lib/pages/AjouterEleve.dart
+++ lib/pages/AjouterEleve.dart
@@ -8,120 +8,126 @@ class AjouterEleve extends StatefulWidget {
 }
 
 class _AjouterEleveState extends State<AjouterEleve> {
-  FocusScopeNode focusScopeNode = FocusScopeNode();
-  TextEditingController matriculeController = TextEditingController();
-  TextEditingController nomController = TextEditingController();
-  TextEditingController prenomController = TextEditingController();
-  TextEditingController classeController = TextEditingController();
-  TextEditingController moyMathController = TextEditingController();
-  TextEditingController moyInfoController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  @override
-  void dispose() {
-    matriculeController.dispose();
-    nomController.dispose();
-    prenomController.dispose();
-    classeController.dispose();
-    moyMathController.dispose();
-    moyInfoController.dispose();
-    super.dispose();
-  }
+  String matricule = '';
+  String nom = '';
+  String prenom = '';
+  String classe = '';
+  String moyMath = '';
+  String moyInfo = '';
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: (){
-          focusScopeNode.unfocus();
-        },
-        child : Center(
-              child: FocusScope(
-                node : focusScopeNode,
-                child : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Saisir infos étudiant',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                  SizedBox(height: 20.0),
-                  Row(
-                    children: [
-                      Text('Matricule:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: matriculeController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Text('Nom:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: nomController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Text('Prénom:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: prenomController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Text('Classe:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: classeController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Text('Moy Math:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: moyMathController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Text('Moy Info:'),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: TextField(
-                          controller: moyInfoController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20.0),
-                ],
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Matricule',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value != null){
+                    if (value.isEmpty) {
+                      return 'Veuillez entrer le matricule';
+                    }
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  matricule = (value != null) ? value : '';
+                },
               ),
-            ) )
-    )
-
-      ;
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nom',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  return (value != null) && (value.isEmpty) ? 'Veuillez entrer le nom' : null;
+                },
+                onSaved: (value) {
+                  nom = (value != null) ? value : '';
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Prénom',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer le prénom' : null;
+                },
+                onSaved: (value) {
+                  prenom = (value != null) ? value : '';
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Classe',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la classe' : null;
+                },
+                onSaved: (value) {
+                  classe = (value != null) ? value : '';
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Moyenne Math',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Math' : null;
+                },
+                onSaved: (value) {
+                  moyMath = (value != null) ? value : '';
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Moyenne Info',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Info' : null;
+                },
+                onSaved: (value) {
+                  moyInfo = (value != null) ? value : '';
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      // Utilisez les valeurs récupérées ici (matricule, nom, prenom, classe, moyMath, moyInfo)
+                      // par exemple, vous pouvez les afficher dans la console :
+                      print('Matricule: $matricule');
+                      print('Nom: $nom');
+                      print('Prénom: $prenom');
+                      print('Classe: $classe');
+                      print('Moyenne Math: $moyMath');
+                      print('Moyenne Info: $moyInfo');
+                    }
+                },
+                child: Text('Valider'),
+              ),
+            ],
+          ),
+        )
+    );
   }
 }
