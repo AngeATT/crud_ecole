bda53cfa28dfb65164b31f14d4814cac80a5a14f
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-05-24 14:14:33 +0000

    maj bd

---
+++ lib/bd/models/Parcour.dart
@@ -0,0 +1,10 @@
+class Parcour{
+  final String id;
+  final String code;
+  final String libelle;
+  const Parcour({
+    required this.id,
+    required this.code,
+    required this.libelle,
+  });
+}
