f97048a32823ae81bc11c08327a3bba5bde313e9
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-05-24 10:26:49 +0000

    ajout du scroll view

--- lib/pages/AjouterEleve.dart
+++ lib/pages/AjouterEleve.dart
@@ -19,115 +19,125 @@ class _AjouterEleveState extends State<AjouterEleve> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Matricule',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value != null){
-                    if (value.isEmpty) {
-                      return 'Veuillez entrer le matricule';
-                    }
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  matricule = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Nom',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  return (value != null) && (value.isEmpty) ? 'Veuillez entrer le nom' : null;
-                },
-                onSaved: (value) {
-                  nom = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Prénom',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer le prénom' : null;
-                },
-                onSaved: (value) {
-                  prenom = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Classe',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la classe' : null;
-                },
-                onSaved: (value) {
-                  classe = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Moyenne Math',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Math' : null;
-                },
-                onSaved: (value) {
-                  moyMath = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Moyenne Info',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Info' : null;
-                },
-                onSaved: (value) {
-                  moyInfo = (value != null) ? value : '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      // Utilisez les valeurs récupérées ici (matricule, nom, prenom, classe, moyMath, moyInfo)
-                      // par exemple, vous pouvez les afficher dans la console :
-                      print('Matricule: $matricule');
-                      print('Nom: $nom');
-                      print('Prénom: $prenom');
-                      print('Classe: $classe');
-                      print('Moyenne Math: $moyMath');
-                      print('Moyenne Info: $moyInfo');
-                    }
-                },
-                child: Text('Valider'),
-              ),
-            ],
-          ),
-        )
+    return ListView(
+      children: <Widget>[
+    Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Form(
+    key: _formKey,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+    Text(
+    'Saisir infos étudiant',
+    style: TextStyle(fontSize: 24.0),
+    ),
+    SizedBox(height: 20.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Matricule',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    if (value != null){
+    if (value.isEmpty) {
+    return 'Veuillez entrer le matricule';
+    }
+    }
+    return null;
+    },
+    onSaved: (value) {
+    matricule = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Nom',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    return (value != null) && (value.isEmpty) ? 'Veuillez entrer le nom' : null;
+    },
+    onSaved: (value) {
+    nom = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Prénom',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer le prénom' : null;
+    },
+    onSaved: (value) {
+    prenom = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Classe',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la classe' : null;
+    },
+    onSaved: (value) {
+    classe = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Moyenne Math',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Math' : null;
+    },
+    onSaved: (value) {
+    moyMath = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Moyenne Info',
+    border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+    return  (value != null) && (value.isEmpty)  ? 'Veuillez entrer la moyenne en Info' : null;
+    },
+    onSaved: (value) {
+    moyInfo = (value != null) ? value : '';
+    },
+    ),
+    SizedBox(height: 16.0),
+    ElevatedButton(
+    onPressed: () {
+    if (_formKey.currentState!.validate()) {
+    _formKey.currentState!.save();
+    // Utilisez les valeurs récupérées ici (matricule, nom, prenom, classe, moyMath, moyInfo)
+    // par exemple, vous pouvez les afficher dans la console :
+    print('Matricule: $matricule');
+    print('Nom: $nom');
+    print('Prénom: $prenom');
+    print('Classe: $classe');
+    print('Moyenne Math: $moyMath');
+    print('Moyenne Info: $moyInfo');
+    }
+    },
+    child: Text('Valider'),
+    ),
+    ],
+    ),
+    )
+    )
+      ],
     );
+
   }
 }
