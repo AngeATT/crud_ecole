02dce1aea4988dd2210802b4e4ceef406c59cd2a
Ismaël 💯 <ismael@Ismael-s-MacBook-Air.local>
2023-05-25 21:41:15 +0000

    my commit

--- lib/pages/Etudiant/AjouterEtudiant.dart
+++ lib/pages/Etudiant/AjouterEtudiant.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
-import '../../textinputformatters/CustomMaxValueInputFormatter.dart';
 import '../../textinputformatters/DecimalTextInputFormatter.dart';
 import '../../textinputformatters/NameTextInputFormatter.dart';
 
